0612584483d32aebf97b10744a78f1c9dce62deb
unknown <tug63585@protonmail.com>
2021-05-23 22:41:08 -0400

    Adding the navigation tab for signin and logout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ParticlesJS from './components/Particles/Particles';
@@ -6,20 +7,34 @@ import 'tachyons';
 var url = require('url');
 
 
+{/*
+  Feeding API key to use the clarifai pretrained models
+*/}
+const app = new Clarifai.App({
+  apiKey: "4a029d6b296d4889b93d1c2bf9519031",
+ });
 
 
-
-function App() {
-  return (
-    <div className="App">
-    <Navigation/>
-    <ParticlesJS/>
-      
-      {/*<Logo/>
-      <ImageLinkForm/>
-      <CelebrityRecognition/>*/}
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input: '',
+      imageUrl: '',
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <Navigation/>
+        <ParticlesJS/>
+          
+          {/*<Logo/>
+          <ImageLinkForm/>
+          <CelebrityRecognition/>*/}
+      </div>
+    ); 
+  }
 }
 
 export default App;
