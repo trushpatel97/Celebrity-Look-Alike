009f89c5c0b178787681fd959496ee2c91240b8b
Trush <trushpatel1997@gmail.com>
2025-06-20 23:20:46 -0400

    test

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Tilt from 'react-tilt'
+import Tilt from 'react-parallax-tilt';
 import './Logo.css'
 import person from './qmark.png';
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 30 }} style={{ height: 100, width: 100 }} >
+            <Tilt className="Tilt br2 shadow-2 " options={{ max : 30 }} style={{ height: 100, width: 100 }} >
                 <div className="Tilt-inner pa3"> 
                     <img style={{paddingTop: '5px'}}src={person} alt="logo"/> 
                 </div>
